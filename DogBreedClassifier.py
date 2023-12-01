556af77293847c016d0699e06aacfe5be62c82f7
Got92 <gauthier@famille-simon.fr>
2023-12-01 18:13:05 +0100

    ajout du code exemple.py
    Ce code fonctionne bien, trouve toutes les images, réussi à bien trier les ensembles.

--- DogBreedClassifier.py
+++ DogBreedClassifier.py
@@ -9,7 +9,7 @@ data_path = "Images"
 
 target_size = (224, 224)  # Taille cible pour redimensionner les images
 
-batch_size = 1000  # Nombre d'images par lot car sans lot, fichier trop lourd 
+batch_size = 100  # Nombre d'images par lot car sans lot, fichier trop lourd 
 
 images = []
 labels = []
@@ -25,12 +25,16 @@ def process_batch(batch_images, batch_labels):
     encoded_labels = label_encoder.fit_transform(batch_labels)
     one_hot_labels = to_categorical(encoded_labels)
 
+    print("Nombre d'images chargées :", len(batch_images))
+    print("Nombre d'étiquettes associées :", len(batch_labels))
+    
     # On vérifie avec des prints que tout se passe correctement (encodage des labels et forme des images)
-    print("Forme des images :", batch_images.shape)
-    print("Forme des labels encodés en one-hot :", one_hot_labels.shape)
+    
+    #print("Forme des images :", batch_images.shape)
+    #print("Forme des labels encodés en one-hot :", one_hot_labels.shape)
 
     # On divise les données en ensembles d'entraînement et de test (80% / 20%)
-    train_images, test_images, train_labels, test_labels = train_test_split(images, one_hot_labels, test_size=0.2, random_state=42)
+    train_images, test_images, train_labels, test_labels = train_test_split(batch_images, one_hot_labels, test_size=0.2, random_state=42)
 
     # On divise l'ensemble d'entraînement en ensembles d'entraînement et de validation (80% / 20%)
     train_images, val_images, train_labels, val_labels = train_test_split(train_images, train_labels, test_size=0.2, random_state=42)
@@ -45,23 +49,31 @@ def process_batch(batch_images, batch_labels):
 for i, breed in enumerate(breed_names):
     breed_folder = os.path.join(data_path, breed)
     if os.path.isdir(breed_folder):
-        print(f"Chargement des images de {breed}")
+        #print(f"Chargement des images de {breed}")
         batch_images = []
         batch_labels = []
+        count_loaded_images = 0
         for img_file in os.listdir(breed_folder):
             img_path = os.path.join(breed_folder, img_file)
-            img = cv2.imread(img_path)
-            if img is not None:
-                img = cv2.resize(img, target_size)  # Redimensionne les images à la taille cible
-                img = img / 255.0  # Normalise les valeurs des pixels entre 0 et 1
-                batch_images.append(img)
-                batch_labels.append(breed)
+            try:
+                img = cv2.imread(img_path)
+                if img is not None:
+                    img = cv2.resize(img, target_size)
+                    img = img / 255.0
+                    batch_images.append(img)
+                    batch_labels.append(breed)
+                    count_loaded_images += 1
+                else:
+                    print(f"Impossible de charger {img_path}")
+            except Exception as e:
+                print(f"Erreur lors du chargement de {img_path}: {e}")
                 
-                # Si la taille du lot est atteinte, le lot est traité ici
-                if len(batch_images) >= batch_size:
-                    process_batch(batch_images, batch_labels)
-                    batch_images = []
-                    batch_labels = []
+            # Si la taille du lot est atteinte, le lot est traité ici
+            if len(batch_images) >= batch_size:
+                process_batch(batch_images, batch_labels)
+                batch_images = []
+                batch_labels = []
+        print(f"Nombre total d'images chargées pour {breed}: {count_loaded_images}")
 
 # On traite les images restantes qui ne constituent pas un lot complet
 if len(batch_images) > 0:
---
+++ exemple.py
@@ -0,0 +1,88 @@
+import os
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+data_path = "Images"
+target_size = (224, 224)
+batch_size = 32
+
+# Créer un générateur d'images
+datagen = ImageDataGenerator(rescale=1./255)
+image_generator = datagen.flow_from_directory(
+    data_path,
+    target_size=target_size,
+    batch_size=batch_size,
+    class_mode='categorical',
+    shuffle=True
+)
+
+# Obtenez le nombre total d'images
+num_images = len(image_generator.filenames)
+
+# Récupérer les labels
+label_encoder = LabelEncoder()
+labels = image_generator.classes
+label_encoder.fit(labels)
+num_classes = len(label_encoder.classes_)
+
+# Diviser les données en ensembles d'entraînement, de validation et de test
+train_ratio = 0.8
+val_ratio = 0.1
+test_ratio = 0.1
+
+train_size = int(num_images * train_ratio)
+val_size = int(num_images * val_ratio)
+
+train_data = []
+val_data = []
+test_data = []
+
+for i in range(num_images // batch_size):
+    batch_images, batch_labels = image_generator.next()
+    if len(train_data) < train_size:
+        train_data.extend(batch_images)
+    elif len(val_data) < val_size:
+        val_data.extend(batch_images)
+    else:
+        test_data.extend(batch_images)
+
+train_images = np.array(train_data)
+val_images = np.array(val_data)
+test_images = np.array(test_data)
+
+train_labels = label_encoder.transform(labels[:len(train_images)])
+val_labels = label_encoder.transform(labels[len(train_images):len(train_images) + len(val_images)])
+test_labels = label_encoder.transform(labels[len(train_images) + len(val_images):])
+
+print("Taille de l'ensemble d'entraînement :", len(train_images))
+print("Taille de l'ensemble de validation :", len(val_images))
+print("Taille de l'ensemble de test :", len(test_images))
+
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+
+# Création du modèle CNN
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+# Compilation du modèle
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Entraînement du modèle
+history = model.fit(train_images, train_labels, epochs=10, batch_size=32, validation_data=(val_images, val_labels))
+
+# Évaluation du modèle sur l'ensemble de test
+test_loss, test_accuracy = model.evaluate(test_images, test_labels)
+print("Précision sur l'ensemble de test :", test_accuracy)
