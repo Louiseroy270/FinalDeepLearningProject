56c33ae31e8820acdf3115dc4b6cdccd3464f0a3
Got92 <gauthier@famille-simon.fr>
2024-01-08 11:10:56 +0100

    Changes in test.py

--- test.py
+++ test.py
@@ -204,21 +204,17 @@ def test(loaders, model, criterion, use_cuda):
 
     model.eval()
     for batch_idx, (data, target) in enumerate(loaders['test']):
-        # move to GPU
         if use_cuda:
             data, target = data.cuda(), target.cuda()
-        # forward pass: compute predicted outputs by passing inputs to the model
+        
         output = model(data)       
-        # calculate the loss
         loss = criterion(output, target)
-        # update average test loss 
+        
         test_loss = test_loss + ((1 / (batch_idx + 1)) * (loss.data - test_loss))
         
-        # convert output probabilities to predicted class
         output = F.softmax(output, dim=1)
         pred = output.data.max(1, keepdim=True)[1]
         
-        # compare predictions to true label
         correct += np.sum(np.squeeze(pred.eq(target.data.view_as(pred))).cpu().numpy())
         total += data.size(0)
             
@@ -227,7 +223,7 @@ def test(loaders, model, criterion, use_cuda):
 
 test(loaders_transfer, model_transfer, criterion_transfer, use_cuda)
 
-class_names = [item[4:].replace("_", " ") for item in train_dataset.classes]
+# class_names = [item[4:].replace("_", " ") for item in train_dataset.classes]
 
 model_transfer.load_state_dict(torch.load('model_transfer.pt'))
 
